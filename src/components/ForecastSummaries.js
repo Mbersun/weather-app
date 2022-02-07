38ea79658ec0ef99c01df0fe773db540bbda19e0
mbersun <bersunmanavoglu@gmail.com>
2022-02-07 03:40:09 +0300

    v1.0

---
+++ src/components/ForecastSummaries.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ForecastSummary from "./ForecastSummary";
+import "../styles/ForecastSummaries.css";
+
+function ForecastSummaries({ forecasts, onForecastSelect }) {
+  return (
+    <div className="forecast-summaries">
+      {forecasts.map((forecast) => (
+        <ForecastSummary
+          key={forecast.date}
+          date={forecast.date}
+          description={forecast.description}
+          icon={forecast.icon.toString()}
+          temperature={forecast.temperature}
+          onSelect={onForecastSelect}
+        />
+      ))}
+    </div>
+  );
+}
+
+ForecastSummaries.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      icon: PropTypes.string.isRequired,
+      temperature: PropTypes.shape({
+        min: PropTypes.number,
+        max: PropTypes.number,
+      }).isRequired,
+    })
+  ).isRequired,
+  onForecastSelect: PropTypes.func.isRequired,
+};
+
+export default ForecastSummaries;
---
+++ src/requests/GetForecast.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+import axios from "axios";
+
+const getForecast = (
+  searchText,
+  setSelectedDate,
+  setForecasts,
+  setLocation,
+  setErrorMessage
+) => {
+  let endpoint = "https://mcr-codes-weather-app.herokuapp.com/forecast";
+
+  if (searchText) {
+    endpoint += `?city=${searchText}`;
+  }
+  axios
+    .get(endpoint)
+    .then((response) => {
+      setErrorMessage("");
+      setSelectedDate(response.data.forecasts[0].date);
+      setForecasts(response.data.forecasts);
+      setLocation(response.data.location);
+    })
+    .catch((error) => {
+      const { status } = error.response;
+      if (status === 404) {
+        setErrorMessage("City is not found");
+        console.error("City is not found", error);
+      }
+      if (status === 500) {
+        setErrorMessage("Server error");
+        console.error("Server error", error);
+      }
+    });
+};
+export default getForecast;
